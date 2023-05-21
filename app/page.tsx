ae1cdffc75065119b012ba7ef904a49fd18cbc9e
Marco Avelar <marco.avelar5@yahoo.com.mx>
2023-05-21 01:44:55 -0500

    first initial working version, code is meh though

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,214 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React from "react";
+import { useState, useEffect } from "react";
+
+import "./main.css";
+
+interface CardInfo {
+  id: number;
+  title: string;
+  description: string;
+  shouldDelete: Boolean;
+}
+
+let draggedCard: CardInfo;
+
+function Card(props: any): JSX.Element {
+  const [grabbed, setGrabbed] = useState<Boolean>(false);
+  const [title, setTitle] = useState<string>(props.cardInfo.title);
+  const [description, setDescription] = useState<string>(
+    props.cardInfo.description
+  );
+  const [titleFlag, setTitleFlag] = useState<Boolean>(true);
+  const [descriptionFlag, setDescriptionFlag] = useState<Boolean>(true);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div
+      onDragStart={() => {
+        setGrabbed(true);
+        props.cardInfo.shouldDelete = false;
+        draggedCard = props.cardInfo;
+      }}
+      onDragEnd={() => {
+        setGrabbed(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        if (draggedCard.shouldDelete) {
+          props.callback(props.cardInfo);
+        } else {
+          draggedCard.shouldDelete = true;
+        }
+      }}
+      className={grabbed ? "cardClass grabbed" : "cardClass"}
+      draggable={true}
+    >
+      <div>
+        <i onClick={() => props.callback(props.cardInfo)} className="rightIcon">
+          &#10006;
+        </i>
+        <i className="leftIcon">=</i>
+      </div>
+      {titleFlag ? (
+        <h5 onClick={() => setTitleFlag(false)}>{title}</h5>
+      ) : (
+        <input
+          autoFocus={true}
+          onKeyDown={(e: any) => {
+            if (e.key == "Enter") {
+              setTitle(e.target.value);
+              props.cardInfo.title = title;
+              props.updateCard(props.cardInfo);
+              setTitleFlag(true);
+            }
+          }}
+          defaultValue={title}
+          onBlur={(e: any) => {
+            setTitle(e.target.value);
+            props.cardInfo.title = title;
+            props.updateCard(props.cardInfo);
+            setTitleFlag(true);
+          }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      )}
+      {descriptionFlag ? (
+        <p
+          onClick={() => {
+            setDescriptionFlag(false);
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {description}
+        </p>
+      ) : (
+        <textarea
+          onKeyDown={(e: any) => {
+            if (e.key == "Enter") {
+              setDescription(e.target.value);
+              props.cardInfo.description = description;
+              props.updateCard(props.cardInfo);
+              setDescriptionFlag(true);
+            }
+          }}
+          defaultValue={description}
+          autoFocus={true}
+          onBlur={(e: any) => {
+            setDescription(e.target.value);
+            props.cardInfo.description = description;
+            props.updateCard(props.cardInfo);
+            setDescriptionFlag(true);
+          }}
+        />
+      )}
+    </div>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function Column(props: any): JSX.Element {
+  const [cards, setCards] = useState<CardInfo[]>([]);
+  const [drag, setDrag] = useState<Boolean>(false);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  useEffect(() => {
+    let items: any = localStorage.getItem(props.title);
+    items = JSON.parse(items);
+    if (items) setCards(items);
+  }, []);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  useEffect(() => {
+    localStorage.setItem(props.title, JSON.stringify(cards));
+  }, [cards]);
+
+  const updateCard = (toUpdate: CardInfo) => {
+    let filtered = cards.map((card: CardInfo) => {
+      if (card.id == toUpdate.id) {
+        card.title = toUpdate.title;
+        card.description = toUpdate.description;
+      }
+      return card;
+    });
+
+    setCards(filtered);
+  };
+
+  function removeFromArr(toDelete: CardInfo) {
+    let filtered = cards.filter((card) => card.id !== toDelete.id);
+
+    setCards(filtered);
+  }
+
+  function addElement() {
+    let currId = props.getAndIncrement();
+
+    setCards([
+      ...cards,
+      {
+        id: currId,
+        title: "Sample Title" + currId.toString(),
+        description: "Sample Description",
+        shouldDelete: true,
+      },
+    ]);
+  }
+
+  return (
+    <div
+      className={drag ? "hoveringColumn" : ""}
+      onDragOver={(e) => {
+        e.preventDefault();
+        setDrag(true);
+      }}
+      onDragLeave={() => {
+        setDrag(false);
+      }}
+      onDrop={() => {
+        setDrag(false);
+        for (const card of cards) {
+          if (card.id == draggedCard.id)
+            return (draggedCard.shouldDelete = false);
+        }
+
+        setCards([...cards, draggedCard]);
+        draggedCard.shouldDelete = true;
+      }}
+    >
+      <h3>{props.title}</h3>
+      <button onClick={() => addElement()}>+</button>
+      <hr />
+      {cards.map((card) => (
+        <Card
+          updateCard={updateCard}
+          callback={removeFromArr}
+          key={card.id}
+          cardInfo={card}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default function Grid(): JSX.Element {
+  const [nextId, setNextId] = useState<number>(0);
+
+  useEffect(() => {
+    const items = localStorage.getItem("nextId");
+
+    if (items) setNextId(parseInt(items));
+    else setNextId(0);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("nextId", JSON.stringify(nextId));
+  }, [nextId]);
+
+  const getAndIncrement = (): number => {
+    setNextId(nextId + 1);
+    return nextId;
+  };
+
+  return (
+    <div className="gridContainer">
+      <Column title="TODO" getAndIncrement={getAndIncrement} />
+      <Column title="Started" getAndIncrement={getAndIncrement} />
+      <Column title="Done" getAndIncrement={getAndIncrement} />
+      <Column title="Deleted" getAndIncrement={getAndIncrement} />
+    </div>
+  );
 }
